517bdd4ad2b1779078bd83a9794306519a128be0
Gustavo Salvo Lara <gsalvo@vasal.cl>
2020-05-21 18:48:05 -0400

    test google drive

--- src/pages/rpdf2/RPDF2.js
+++ src/pages/rpdf2/RPDF2.js
@@ -1,11 +1,31 @@
 import React from 'react';
 
-function RPDF2() {
-  return (
-    <div className="content">
-      <p>rpdf2</p>
-    </div>
-  );
+class RPDF2 extends React.Component {
+  
+  constructor(){
+    super();
+    this.iframeElement = React.createRef();
+  }
+
+  componentDidMount() {
+    console.log(this.iframeElement);
+    //this.iframe.getElementsByTagName("img")[0].style.width="100%";
+  } 
+
+  render() {
+    return (
+      <div className="content">
+        <iframe 
+          id="theFrame" 
+          src="https://www.eurofound.europa.eu/sites/default/files/ef_publication/field_ef_document/ef1710en.pdf" 
+          height="300" 
+          width="300"
+          ref={this.iframeElement} 
+        >
+        </iframe>
+      </div>
+    );
+  }
 }
 
 export default RPDF2;
--- src/pages/simple/Simple.js
+++ src/pages/simple/Simple.js
@@ -3,21 +3,7 @@ import React from 'react';
 function Simple() {
   return (
     <div className="content">
-      <object
-        data="https://www.eurofound.europa.eu/sites/default/files/ef_publication/field_ef_document/ef1710en.pdf"
-        type="application/pdf"
-        width="100%"
-        height="100%"
-      >
-        <iframe
-          src="https://www.eurofound.europa.eu/sites/default/files/ef_publication/field_ef_document/ef1710en.pdf"
-          width="320"
-          height="500"
-        >
-          <p>Your browser does not support PDFs.
-      <a href="https://www.eurofound.europa.eu/sites/default/files/ef_publication/field_ef_document/ef1710en.pdf">Download the PDF</a>.</p>
-        </iframe>
-      </object>
+      <iframe src="https://drive.google.com/viewerng/viewer?url=https://www.eurofound.europa.eu/sites/default/files/ef_publication/field_ef_document/ef1710en.pdf?pid=explorer&efh=false&a=v&chrome=false&embedded=true" width="400px" height="300px"  />      
     </div>
   );
 }
